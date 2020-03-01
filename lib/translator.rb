140e53fd3c38b41d6ee43a37b33b47c1407ff0e6
Sara Khandaker <kttsara@hotmail.com>
2020-03-01 07:16:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,19 +15,19 @@ end
 def get_japanese_emoticon (path, emo)
   n=load_library(path)
 jap_emo=nil
-  n[:get_emoticon].each_key{|key|
-    jap_emo=n[:get_emoticon][key][1] if n[:get_emoticon][key][0]==emo
-  }
-  jap_emo
-  "Sorry, that emoticon was not found" if jap_emo=nil
+ # n[:get_emoticon].each_key{|key|
+ #   jap_emo=n[:get_emoticon][key][1] if n[:get_emoticon][key][0]==emo
+ # }
+ # jap_emo
+ # "Sorry, that emoticon was not found" if jap_emo=nil
 end
 
 def get_english_meaning (path, emo)
   n=load_library(path)
 jap_emo=nil
-  n[:get_emoticon].each_key{|key|
-    jap_emo=n[:get_emoticon][key][1] if n[:get_emoticon][key][0]==emo
-  }
-  jap_emo
+ # n[:get_emoticon].each_key{|key|
+ #   jap_emo=n[:get_emoticon][key][1] if n[:get_emoticon][key][0]==emo
+ # }
+ # jap_emo
   "Sorry, that emoticon was not found" if jap_emo=nil
 end
