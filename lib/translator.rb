c25799f6ba34ddb3f28395d3925ca10232a73c9f
Sara Khandaker <kttsara@hotmail.com>
2020-03-01 07:06:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_english_meaning (path, emo)
   n=load_library(path)
 jap_emo=nil
   n[:get_emoticon].each_key{|key|
-    jap_emo=n[:get_emoticon][key][1] if n:[get_emoticon][key][0]==emo
+    jap_emo=n[:get_emoticon][key][1] if n[:get_emoticon][key][0]==emo
   }
   jap_emo
   "Sorry, that emoticon was not found" if jap_emo=nil
