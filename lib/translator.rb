89876b327a33f30ad468312d7ec7b1c77bc47949
Sara Khandaker <kttsara@hotmail.com>
2020-03-01 06:51:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,34 @@
 # require modules here
-
-def load_library
-  # code goes here
+require "yaml"
+require "pry"
+def load_library (path)
+  n=YAML.load_file(path)
+j=[]
+e=[]
+n.each_key{|meaning|
+j<< {n[meaning]=> meaning}
+e<< {meaning=> n[meaning]}
+}
+newhash={:get_meaning=>j,
+:get_emoticon=>e}
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (path, emo)
+  n=load_library(path)
+jap_emo=nil
+  n.each_key{|key|
+    jap_emo=n[key][1] if n[key][0]==emo
+  }
+  jap_emo
+  "Sorry, that emoticon was not found" if jap_emo=nil
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning (path, emo)
+  n=load_library(path)
+  jap_emo=nil
+  n.each_key{|key|
+    jap_emo=n[key][0] if n[key][1]==emo
+  }
+  jap_emo
+  "Sorry, that emoticon was not found" if jap_emo=nil
 end
